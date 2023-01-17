345a6067a10c0afe1eee696b4ccf160af2011c6d
nsemirdev <nsemirdev@gmail.com>
2023-01-17 16:14:12 +0300

    tabbar configuration

--- Trendyol-VIPER/Scenes/Core/TabBar/TYTabBarController.swift
+++ Trendyol-VIPER/Scenes/Core/TabBar/TYTabBarController.swift
@@ -7,9 +7,54 @@
 
 import UIKit
 
+/// Initial controller for App
 final class TYTabBarController: UITabBarController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        configureAppearance()
+    }
+    
+    private func configureAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().tintColor = .orange
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("TabBarLoaded")
+        view.backgroundColor = .systemTeal
+        setUpViewControllers()
+    }
+    
+    private func setUpViewControllers() {
+        let viewControllers = [
+            TYHomeFeedViewController(),
+            TYTrendyolGoViewController(),
+            TYFavoritesViewController(),
+            TYBasketViewController(),
+            TYAccountViewController()
+        ]
+        
+        let titles = [
+            "Anasayfa",
+            "Trendyol Go",
+            "Favorilerim",
+            "Sepetim",
+            "Hesabım"
+        ]
+        
+        for idx in 0..<viewControllers.count {
+            viewControllers[idx].tabBarItem.title = titles[idx]
+        }
+        
+        setViewControllers(viewControllers, animated: false)
     }
 }
