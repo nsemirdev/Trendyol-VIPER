2c035c104cbe921e4d6f88127a7fff2c0eab4c3d
nsemirdev <nsemirdev@gmail.com>
2023-01-17 15:36:16 +0300

    added launchscreen design and core tabbar

---
+++ Trendyol-VIPER/Scenes/Core/TabBar/TYTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TYTabBarController.swift
+//  Trendyol-VIPER
+//
+//  Created by Emir Alkal on 17.01.2023.
+//
+
+import UIKit
+
+final class TYTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("TabBarLoaded")
+    }
+}
