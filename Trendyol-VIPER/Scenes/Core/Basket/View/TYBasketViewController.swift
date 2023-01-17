7615fd05ae6f57ac4499604ea8f89b1c5778bb0e
nsemirdev <nsemirdev@gmail.com>
2023-01-17 15:28:58 +0300

    added core view controllers

---
+++ Trendyol-VIPER/Scenes/Core/Basket/View/TYBasketViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TYBasketViewController.swift
+//  Trendyol-VIPER
+//
+//  Created by Emir Alkal on 17.01.2023.
+//
+
+import UIKit
+
+final class TYBasketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
